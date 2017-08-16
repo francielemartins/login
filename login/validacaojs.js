6f8bfbd9ffc971b83e1c28673ebaba7ea7b2227f
Franciele Martins <franciele.martins91@outlook.com>
2017-08-15 22:07:42 -0300

    Validacao JS

---
+++ login/validacaojs.js
@@ -0,0 +1,14 @@
+﻿function enviar() {
+        var email = document.getElementById("email").value;
+        var senha = document.getElementById("senha").value;
+
+            if (email == '' || senha == '') {
+            alert("Preencha todos os campos!");
+        }
+        else if (email == 'email' & senha == 'senha') {
+            alert("Entrou!!!");
+        }
+        else {
+            alert("Usuário ou senha inválidos");
+        }
+        }
