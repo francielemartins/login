a63d8bf1af995dbaf05962b9a21af72886836ea8
Franciele Martins <franciele.martins91@outlook.com>
2017-08-29 21:35:34 -0300

    jquery

--- login/validacaojs.js
+++ login/validacaojs.js
@@ -1,14 +1,32 @@
 ﻿function enviar() {
-        var email = document.getElementById("email").value;
-        var senha = document.getElementById("senha").value;
+    var email = $("#email").val();
+    var senha = $("#senha").val();
 
-            if (email == '' || senha == '') {
-            alert("Preencha todos os campos!");
-        }
-        else if (email == 'email' & senha == 'senha') {
-            alert("Entrou!!!");
-        }
-        else {
-            alert("Usuário ou senha inválidos");
-        }
-        }
+    if (email == '' || senha == '') {
+        alert("Preencha todos os campos!");
+    }
+    else if (email == 'email' & senha == 'senha') {
+        alert("Entrou!!!");
+    }
+    else {
+        alert("Usuário ou senha inválidos");
+    }
+}
+
+
+
+//function enviar() {
+//        var email = document.getElementById("email").value;
+//        var senha = document.getElementById("senha").value;
+
+//            if (email == '' || senha == '') {
+//            alert("Preencha todos os campos!");
+//        }
+//        else if (email == 'email' & senha == 'senha') {
+//            alert("Entrou!!!");
+//        }
+//        else {
+//            alert("Usuário ou senha inválidos");
+//        }
+//}
+
